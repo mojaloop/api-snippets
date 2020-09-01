7b400386d0b9cdad11c2b5113578789d1d30e922
Kevin Leyow <kleyow@gmail.com>
2020-09-01 12:14:28 -0500

    chore: add commit linting

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  header: '# Changelog: [mojaloop/api-snippets](https://github.com/mojaloop/api-snippets)',
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'chore', section: 'Maintenance' },
+    { type: 'ci', section: 'CI/CD' },
+    { type: 'docs', section: 'Documentation' },
+    { type: 'style', section: 'Style Improvements' },
+    { type: 'refactor', section: 'Code Refactor' },
+    { type: 'perf', section: 'Performance' },
+    { type: 'test', section: 'Tests' }
+  ]
+}
