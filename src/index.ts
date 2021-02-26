3d6de2b829c97e1589ea4bf5e62160e5a15d9e14
Paweł Marzec <pm@eoln.eu>
2021-02-26 08:43:17 +0100

    fix: bring back default exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 /* eslint-disable camelcase */
+
+import * as v1_0 from './fspiop/v1_0'
+import * as v1_1 from './fspiop/v1_1'
+import * as thirdparty from './thirdparty'
+
 export * as v1_0 from './fspiop/v1_0'
 export * as v1_1 from './fspiop/v1_1'
 export * as thirdparty from './thirdparty'
+
+export default {
+  v1_0,
+  v1_1,
+  thirdparty
+}
