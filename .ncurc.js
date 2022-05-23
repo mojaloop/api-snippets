8090f60f32c87e442132d3db14cf87012336d7d3
Kevin <kleyow@gmail.com>
2022-05-23 16:49:03 -0500

    chore: add comment

--- .ncurc.js
+++ .ncurc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reject: ['husky']
+  reject: [
+    // husky >4 involves a full config migration with no current and apparent benefit.
+    // So we are just sticking to husky@4.x.x for the time being.
+    'husky'
+  ]
 }
