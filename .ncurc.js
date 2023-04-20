700da55f154f2b1dababeb61cee74d05828e704e
Vijay <vijaya.guthi@infitx.com>
2023-04-20 15:20:42 +0530

    chore: updated deps

--- .ncurc.js
+++ .ncurc.js
@@ -4,6 +4,10 @@ module.exports = {
     // So we are just sticking to husky@4.x.x for the time being.
     'husky',
     // Transpiler currently fails. TODO: Story to investigate and fix.
-    'openapi-typescript'
+    'openapi-typescript',
+    // 10.1.0 is a breaking change and has some issues
+    '@apidevtools/json-schema-ref-parser',
+    // Upgrading typescript to 5.0.4 causing unit test failures due to breaking changes related to `ttypescript` for 'custom transformers'
+    'typescript'
   ]
 }
