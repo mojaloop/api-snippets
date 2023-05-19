630295c9a378c59fc5bd34e091f01dba10353fe9
Vijay <vijaya.guthi@infitx.com>
2023-05-19 14:56:25 +0530

    fix: revert to homeR2PTransactionId in sdk backend transactionRequestResponse

--- src/sdk-scheme-adapter/v2_0_0/backend/openapi.ts
+++ src/sdk-scheme-adapter/v2_0_0/backend/openapi.ts
@@ -557,7 +557,7 @@ export interface components {
     /** @description A response to a request for a quote. */
     transactionRequestResponse: {
       /** @description Transaction ID from the DFSP backend, used to reconcile transactions between the Switch and DFSP backend systems. */
-      homeTransactionId?: string;
+      homeR2PTransactionId?: string;
       transactionId: components["schemas"]["transactionId"];
       transactionRequestState: components["schemas"]["transactionRequestState"];
     };
