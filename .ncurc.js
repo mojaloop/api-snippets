6bd84f944bf7f6588aab0a7bff60de1d47f0f631
Steven Oderayi <oderayi@gmail.com>
2023-08-16 14:30:48 +0100

    chore: (mojaloop/#3475) - clean up .ncurc.js

--- .ncurc.js
+++ .ncurc.js
@@ -1,8 +1,5 @@
 module.exports = {
   reject: [
-    // Upgrading past husky@4 to involves a full config migration with no current and apparent benefit.
-    // So we are just sticking to husky@4.x.x for the time being.
-    //'husky',
     // 10.1.0 is a breaking change and has some issues
     '@apidevtools/json-schema-ref-parser',
   ]
