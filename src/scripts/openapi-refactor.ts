438c01ae2a9a9ac8af6213f0f291c3d565a45b5e
vijayg10 <33152110+vijayg10@users.noreply.github.com>
2023-12-05 22:24:03 +0530

    feat(mojaloop/3483): add fx apis (#169)
    * feat: added browsersync script
    
    * feat:added fxquotes and fxtransfers defs
    
    * fix: refactored fxquotes and fxtransfers
    
    * feat: added other fspiop changes for fx
    
    * fix: some fixes in fspiop fx
    
    * fix: build error
    
    * feat: addressed some comments
    
    * fix: renamed fxquoteid to conversionRequestId
    
    * fix: build
    
    * fix: filename case change
    
    * feat: changed fspiop new version number
    
    * chore: some comments
    
    * fix: added kyc information
    
    * chore: fix audits
    
    * chore: cleaning
    
    * fix: fxmoney
    
    * fix: required fileds in fxquotes callback
    
    * feat: added reordering script
    
    * chore: updated fspiop apis with reording components
    
    * fix: fspiop2.0 reviewed
    
    * chore: deps
    
    * fix: audit

---
+++ src/scripts/openapi-refactor.ts
@@ -0,0 +1,78 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+ Contributors
+ --------------
+ This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+ * Modusbox
+ - Vijay Kumar Guthi <vijaya.guthi@modusbox.com>
+
+ --------------
+ ******/
+
+// Accepts an OpenAPI YAML file as input and alphabetically sorts the components section
+// This is done to maintain a consistent order of components
+
+import fs from 'fs'
+import yaml from 'js-yaml'
+import { stringifyYaml } from '@redocly/openapi-core'
+
+const myArgs = process.argv.slice(2)
+if (myArgs.length !== 2) {
+  console.log('Usage: openapi-sort-components.js <openapi-spec-yaml-file> <output-yaml-file>')
+  process.exit(1)
+}
+const openApiFile = myArgs[0]
+const outputFile = myArgs[1]
+
+const openapi: any = yaml.load(fs.readFileSync(openApiFile, 'utf8'))
+
+if (openapi.components.schemas) {
+  const sortedComponentSchemas: any = {}
+  Object.keys(openapi.components.schemas)
+    .sort()
+    .forEach((key) => {
+      sortedComponentSchemas[key] = openapi.components.schemas[key]
+    })
+  openapi.components.schemas = sortedComponentSchemas
+}
+
+if (openapi.components.parameters) {
+  const sortedComponentParameters: any = {}
+  Object.keys(openapi.components.parameters)
+    .sort()
+    .forEach((key) => {
+      sortedComponentParameters[key] = openapi.components.parameters[key]
+    })
+  openapi.components.parameters = sortedComponentParameters
+}
+
+// Deleting unnecessary paths those are added as a workaround for the openapi-cli bundle issue
+delete openapi.paths['/interface']
+
+fs.writeFileSync(
+  outputFile,
+  stringifyYaml(openapi, {
+    quotingType: '"',
+    forceQuotes: false
+  })
+)
+
+console.log(`Refactored the specification in ${outputFile}`)
