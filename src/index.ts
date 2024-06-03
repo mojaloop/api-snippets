10b994f2dbdc3ed786a3fb12a4874dfa0b2d7a04
geka.evk <geka.evk@gmail.com>
2024-06-03 11:07:09 +0100

    chore: exported fspiop v2_0

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@
 
 import * as v1_0 from './fspiop/v1_0'
 import * as v1_1 from './fspiop/v1_1'
+import * as v2_0 from './fspiop/v2_0'
 import * as thirdparty from './thirdparty'
 import * as SDKSchemeAdapter from './sdk-scheme-adapter'
 
 export * as v1_0 from './fspiop/v1_0'
 export * as v1_1 from './fspiop/v1_1'
+export * as v2_0 from './fspiop/v2_0'
 export * as thirdparty from './thirdparty'
 export * as SDKSchemeAdapter from './sdk-scheme-adapter'
 
 export default {
   v1_0,
   v1_1,
+  v2_0,
   thirdparty,
   SDKSchemeAdapter
 }
