4264f2975731c5fcab1161ce3507efc92bee4d91
Kalin Krustev <kalin.krustev@gmail.com>
2024-06-03 08:15:30 +0000

    chore: deps

--- .ncurc.js
+++ .ncurc.js
@@ -2,5 +2,7 @@ module.exports = {
   reject: [
     // 10.1.0 is a breaking change and has some issues
     '@apidevtools/json-schema-ref-parser',
+    // upgrade to eslint 9 is non-trivial
+    'eslint',
   ]
 }
