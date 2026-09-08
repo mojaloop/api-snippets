226cf89f3d43d29fb52f087d2772372bea88014d
Yui <yuit@users.noreply.github.com>
2026-09-08 11:33:46 -0700

    fix(pisp): use correct version of third-party API specification to build .d.ts file (#225)
    * chore: update dependencies
    
    * chore: remove old v1.0 file that got renamed

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,8 @@ module.exports = {
   reject: [
     // js-yaml 5.x removed the named `types` export, which breaks this repo's build:
     // `The requested module 'js-yaml' does not provide an export named 'types'`.
-    'js-yaml'
+    'js-yaml',
+    // Contain breaking change for ts-node
+    'typescript'
   ]
 }
